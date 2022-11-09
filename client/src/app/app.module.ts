5eec4e2da7ac1d444e5835dc22df1d9abab7afa4
ycheun29 <ycheun29@my.centennialcollege.ca>
2022-11-09 10:29:11 -0500

    rearrange files

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,20 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BookStoreModule } from './book-store/book-store.module';
 import { PagesModule } from './pages/pages.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BookStoreModule,
-    PagesModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, PagesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
