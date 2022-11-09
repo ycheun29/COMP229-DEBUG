928f915e18f15d2528f0deda30804c6c1a69a322
Karlis00 <113260747+Karlis00@users.noreply.github.com>
2022-11-09 16:40:57 -0500

    clear up useless code of view-all-survey

--- client/src/app/partials/view-all-survey/view-all-survey.component.ts
+++ client/src/app/partials/view-all-survey/view-all-survey.component.ts
@@ -11,44 +11,17 @@ export class ViewAllSurveyComponent
 {
   
     public selectedCreator = null;
-    public surveysPerPage = 4;
-    public selectedPage = 1;
   
     constructor(private repository: SurveyRepository) { }
   
     get surveys(): Survey[]
     {
-      const pageIndex = (this.selectedPage - 1) * this.surveysPerPage;
       return this.repository.getAllSurveys()
-      .slice(pageIndex, pageIndex + this.surveysPerPage);
     }
   
     get creators(): string[]
     {
       return this.repository.getCreators();
-    }
-  
-    changeCreator(newCreator?: string): void
-    {
-      this.selectedCreator = newCreator;
-    }
-  
-    changePage(newPage: number): void
-    {
-      this.selectedPage = newPage;
-    }
-  
-    changePageSize(newSize: number): void
-    {
-      this.surveysPerPage = Number(newSize);
-      this.changePage(1);
-    }
-  
-    get pageCount(): number
-    {
-      return Math.ceil(this.repository
-        .getAllSurveys().length / this.surveysPerPage);
-    }
-  
-
+    } 
+ 
 }
