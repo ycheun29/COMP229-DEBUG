e839407f332cb75324d69d5be88535629b85af6b
ycheun29 <ycheun29@my.centennialcollege.ca>
2022-11-11 01:21:53 -0500

    update file structure

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,11 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ProductsComponent } from './pages/products/products.component';
-import { ServicesComponent } from './pages/services/services.component';
 import { SurveySiteComponent } from './survey-site/survey-site.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { CreateSurveyComponent } from './pages/create-survey/create-survey.component';
 import { ViewSurveyComponent } from './pages/view-survey/view-survey.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -14,23 +10,11 @@ import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, data: { title: 'Home' } },
-  { path: 'about', component: AboutComponent, data: { title: 'About' } },
-  {
-    path: 'products',
-    component: ProductsComponent,
-    data: { title: 'Products' },
-  },
-  {
-    path: 'services',
-    component: ServicesComponent,
-    data: { title: 'Services' },
-  },
   {
     path: 'survey-list',
     component: SurveySiteComponent,
     data: { title: 'Survey Site' },
   },
-  { path: 'contact', component: ContactComponent, data: { title: 'Contact' } },
   {
     path: 'create-survey',
     component: CreateSurveyComponent,
--- client/src/app/pages/pages.module.ts
+++ client/src/app/pages/pages.module.ts
@@ -1,35 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
-import { ProductsComponent } from './products/products.component';
-import { ServicesComponent } from './services/services.component';
 import { PartialsModule } from '../partials/partials.module';
 import { CreateSurveyComponent } from './create-survey/create-survey.component';
 import { ViewSurveyComponent } from './view-survey/view-survey.component';
 import { LoginComponent } from './login/login.component';
 
-
 @NgModule({
   imports: [BrowserModule, FormsModule, PartialsModule],
   declarations: [
-    AboutComponent,
-    ContactComponent,
     HomeComponent,
-    ProductsComponent,
-    ServicesComponent,
     CreateSurveyComponent,
     ViewSurveyComponent,
-    LoginComponent
+    LoginComponent,
   ],
-  exports: [
-    AboutComponent,
-    ContactComponent,
-    HomeComponent,
-    ProductsComponent,
-    ServicesComponent,
-    PartialsModule]
+  exports: [HomeComponent, PartialsModule],
 })
 export class PagesModule {}
