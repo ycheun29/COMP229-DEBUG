e88c6357539f53664624ccc9a29f274ce4d5f872
Karlis00 <113260747+Karlis00@users.noreply.github.com>
2022-11-17 15:36:28 -0500

    revamp home page

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -5,7 +5,7 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent extends BasePageComponent implements OnInit {
 
