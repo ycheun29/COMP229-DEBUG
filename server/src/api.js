6f33bae73c91b6f29d889879b0428f008b4106da
ycheun29 <ycheun29@my.centennialcollege.ca>
2022-11-26 18:56:55 -0500

    update api

--- server/src/api.js
+++ server/src/api.js
@@ -1,17 +1,139 @@
-const express = require("express");
+// const express = require("express");
+// const serverless = require("serverless-http");
+
+// const app = express();
+
+// const router = express.Router();
+
+// router.get("/", (req, res) => {
+//   res.json({ hello: "hi!" });
+// });
+// router.get("/test", (req, res) => {
+//   res.json({ hello: "test!" });
+// });
+
+// app.use("/.netlify/functions/api", router);
+
+// module.exports.handler = serverless(app);
+
+// installed 3rd party packages
+let createError = require("http-errors");
+let express = require("express");
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
+let cors = require("cors");
 const serverless = require("serverless-http");
 
-const app = express();
+// modules for authentication
+let session = require("express-session");
+let passport = require("passport");
 
-const router = express.Router();
+let passportJWT = require("passport-jwt");
+let JWTStrategy = passportJWT.Strategy;
+let ExtractJWT = passportJWT.ExtractJwt;
 
-router.get("/", (req, res) => {
-  res.json({ hello: "hi!" });
+let passportLocal = require("passport-local");
+let localStrategy = passportLocal.Strategy;
+let flash = require("connect-flash");
+
+// database setup
+let mongoose = require("mongoose");
+let DB = require("../config/db");
+
+// point mongoose to the DB URI
+mongoose.connect(DB.URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let mongoDB = mongoose.connection;
+mongoDB.on("error", console.error.bind(console, "Connection Error:"));
+mongoDB.once("open", () => {
+  console.log("Connected to MongoDB...");
 });
-router.get("/test", (req, res) => {
-  res.json({ hello: "test!" });
+
+let userRouter = require("../routes/user");
+let surveysRouter = require("../routes/survey");
+
+let app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "ejs"); // express  -e
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../../public")));
+app.use(express.static(path.join(__dirname, "../../node_modules")));
+
+app.use(cors());
+
+//setup express session
+app.use(
+  session({
+    secret: "SomeSecret",
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
+// initialize flash
+app.use(flash());
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport user configuration
+
+// create a User Model Instance
+let userModel = require("../models/user");
+let User = userModel.User;
+
+// implement a User Authentication Strategy
+passport.use(User.createStrategy());
+
+// serialize and deserialize the User info
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = DB.Secret;
+
+let strategy = new JWTStrategy(jwtOptions, (jwt_payload, done) => {
+  User.findById(jwt_payload.id)
+    .then(user => {
+      return done(null, user);
+    })
+    .catch(err => {
+      return done(err, false);
+    });
 });
 
-app.use("/.netlify/functions/api", router);
+passport.use(strategy);
+
+// routing
+app.use("/api/user", userRouter);
+app.use("/api/survey-list", surveysRouter);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error", { title: "Error" });
+});
 
 module.exports.handler = serverless(app);
